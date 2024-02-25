abb16eecf9e17bf47871e7bd7cb1d770cab0c397
Mateus Cintra <mateusdade@gmail.com>
2024-02-25 14:18:06 -0300

    Mudança no chat

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -12,7 +12,7 @@ import { ModeToggle } from "./ModeToggle"
 export interface ChatProps {}
 
 export function Chat() {
-    const { messages, input, handleInputChange, handleSubmit } = useChat();
+
 
     return (
         <Card className="w-[440px]">
@@ -33,37 +33,13 @@ export function Chat() {
         </CardHeader>
         <CardContent>
         <ScrollArea className="h-[300px] w-full pr-4">
-            { messages.map(message => {
-                return(
-                <div key={message.id} className="flex gap-3 text-slate-600 text-sm mb-4">
-                 {message.role === 'user' && (
-                    <Avatar>
-                    <AvatarFallback>User</AvatarFallback>
-                    <AvatarImage src="https://avatars.githubusercontent.com/u/101955322?v=4"/>
-                  </Avatar>
-                 )}
-
-                {message.role === 'assistant' && (
-                    <Avatar>
-                    <AvatarFallback>AI</AvatarFallback>
-                    <AvatarImage src="toby.png"/>
-                  </Avatar>
-                 )}
-
-                
-                <p className="leading-relaxed">
-                  <span className="block font-bold text-slate-700">
-                    {message.role === 'user' ? 'Usuário' : 'Toby'}:</span>
-                  {message.content}
-                </p>
-              </div>
-              )
-            })}
+        
+  
         </ScrollArea>
         </CardContent>
         <CardFooter>
-            <form className="w-full flex gap-2" onSubmit={handleSubmit}>
-                <Input placeholder="Como posso te ajudar?" value={input} onChange={handleInputChange}/>
+            <form className="w-full flex gap-2" >
+                <Input placeholder="Como posso te ajudar?" />
                 <Button type="submit">Enviar</Button>
           </form>
         </CardFooter>
